afef19e58d6808ef1c7c97142ca29fbc250406d6
ajit <github@vyrazu.com>
2014-08-04 21:35:16 +0530

    doc changes initial

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,52 @@
-$(window).load(function(){
-	var vdheight = $("#bgvid").height();
-	$(".banner").css("height", vdheight);
-});
 
 $(document).ready(function(){
-	
-	$(".effctimg").hover(function(){
-		$(this).children('.txt-container').slideToggle("slow");
-	});
-	
-	$(".img-flip").hover(function(){
-		$(this).children('.txt-flip').slideToggle("slow");
-	});
-	
+// 	
+	// $(".effctimg").hover(function(){
+		// $(this).children('.txt-container').slideToggle("slow");
+	// });
+// // 	
+	// $(".img-flip").hover(function(){
+		// $(this).children('.txt-flip').slideToggle("slow");
+	// });
+// 	
 	skrollr.init();
 	
 });
+
+$(window).scroll(function(){
+	
+	if ($(window).scrollTop() > 150) 
+	{
+		$('.nav-custom').addClass('navcustomht');
+	}
+	else 
+	{
+		$('.nav-custom').removeClass('navcustomht');
+	}
+	
+	
+	
+});
+
+$(function(){
+        $.okvideo({ source: 'https://www.youtube.com/watch?v=pRCelvhci7A', 
+                    volume: 100, 
+                    loop: true,
+                    hd:true, 
+                    adproof: true,
+		   // disablekeyControl: true;
+                    annotations: false,
+                    onFinished: function() { console.log('finished') },
+                    unstarted: function() { console.log('unstarted') },
+                    onReady: function() { console.log('onready') },
+                    onPlay: function() { console.log('onplay') },
+                    onPause: function() { console.log('pause') },
+                    buffering: function() { console.log('buffering') },
+                    cued: function() { console.log('cued') },
+                 });
+                 
+       $('.banner').css({
+            height: $(window).height()
+        });
+                 
+    });
