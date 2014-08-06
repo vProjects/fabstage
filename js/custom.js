714a4b3f9b6b689901d4ac81c5c4f20e07ecd8b6
ajit <github@vyrazu.com>
2014-08-06 15:00:50 +0530

    final changes video and fb

--- js/custom.js
+++ js/custom.js
@@ -28,25 +28,28 @@ $(window).scroll(function(){
 		$('.navbar-brand img').css('margin-top','0px');
 	}
 	
+	
+	/*
+	 * assigning relative position instead of fixed position of video 
+	 */
+	
+	// get window position from top while scrolling
+	var wPos = $(window).scrollTop(); 
+	//get end position of banner from Top
+	var banner = $('.banner').offset().top + $('.banner').height();
+	//check if the banner with the background video is scrolled up out of the view and change the css of the video 
+	if (wPos >= banner) {
+		$('#wrapper_mbYTP_bgndVideo').css("position","relative");
+	}
+	else 
+	{
+		$('#wrapper_mbYTP_bgndVideo').css("position","fixed");
+	}
+	
 });
 
 $(function(){
-        // $.okvideo({ source: 'EpbjEttizy8', 
-                    // volume: 100, 
-                    // loop: true,
-                    // hd:true, 
-                    // adproof: true,
-                    // autoplay: false,
-		   			// // disablekeyControl: true;
-                    // annotations: false,
-                    // onFinished: function() { console.log('finished') },
-                    // unstarted: function() { console.log('unstarted') },
-                    // onReady: function() { console.log('onready') },
-                    // onPlay: function() { console.log('onplay') },
-                    // onPause: function() { console.log('pause') },
-                    // buffering: function() { console.log('buffering') },
-                    // cued: function() { console.log('cued') },
-                 // });
+       
                  
        $('.banner').css({
             height: $(window).height()
@@ -63,9 +66,11 @@ jQuery(function(){
                 jQuery("#eventListener").append(" :: (quality= "+ jQuery("#bgndVideo").getPlayer().getPlaybackQuality()+")");
             });
             jQuery("#bgndVideo").on("YTPLoop", function(e){
+            	console.log(jQuery("#eventListener").val());
                 jQuery("#eventListener").html("YTPLoop");
                 jQuery("#eventListener").append(" :: (state= "+ jQuery("#bgndVideo").getPlayer().getPlayerState()+")");
                 jQuery("#eventListener").append(" :: "+ e.counter);
+                
             });
             jQuery("#bgndVideo").on("YTPEnd", function(){
                 jQuery("#eventListener").html("YTPEnd");
